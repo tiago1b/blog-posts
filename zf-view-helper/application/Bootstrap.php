5422b6aead2ff3ea5e591ce1aef9f6cfb105e4ac
Felipe Djinn <lfrs_web@yahoo.com.br>
2011-05-10 12:14:39 -0300

    Add zf view helper post codes

---
+++ zf-view-helper/application/Bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/** application/Bootstrap.php **/
+
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+    /**
+    public function _initHelpers()
+    {
+        $viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('viewRenderer');
+        $viewRenderer->initView();
+
+        $viewRenderer->view->addHelperPath('ZF/View/Helper/', 'ZF_View_Helper');
+    }
+    **/
+
+}
